ba13c4a653490def68cf2ce5665d1ea50426ce95
Marjory.BRAVO@fittingbox.com <marjory.bravo@fittingbox.com>
2025-04-15 10:25:45 +0200

    feat: add authguard and isadminguard

--- src/offer/offer.controller.ts
+++ src/offer/offer.controller.ts
@@ -6,15 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { OfferService } from './offer.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { UpdateOfferDto } from './dto/update-offer.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { IsAdminGuard } from 'src/user/isAdmin.guard';
 
 @Controller('offer')
 export class OfferController {
   constructor(private readonly offerService: OfferService) {}
 
+  @UseGuards(AuthGuard, IsAdminGuard)
   @Post()
   create(@Body() createOfferDto: CreateOfferDto) {
     return this.offerService.create(createOfferDto);
@@ -30,16 +34,19 @@ export class OfferController {
     return this.offerService.findOne(id);
   }
 
+  @UseGuards(AuthGuard, IsAdminGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
     return this.offerService.update(id, updateOfferDto);
   }
 
+  @UseGuards(AuthGuard, IsAdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.offerService.remove(id);
   }
 
+  @UseGuards(AuthGuard, IsAdminGuard)
   @Post('/upload')
   createImg(@Body() formData: FormData) {
     // upload file in a service or middleware
